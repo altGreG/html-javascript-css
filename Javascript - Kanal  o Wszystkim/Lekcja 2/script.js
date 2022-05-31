c1eca5fe988a75373f926a60665bb4c1ef4280c9
altGreG <grzeg.bak.pl@gmail.com>
2022-05-31 11:27:53 +0200

    Początek nauki metodologii Bem oraz ukończenie pierwszego oraz drugiego odcinka kursu Javascript

---
+++ Javascript - Kanal  o Wszystkim/Lekcja 2/script.js
@@ -0,0 +1,50 @@
+console.log("Hello World!");
+
+// Operatory arytmetyczne
+
+var x = 2;
+console.log(x + 65);
+console.log(x - 2);
+console.log(x * 5);
+console.log(5 / x);
+console.log(5 % x);
+
+console.log("5" + 2 + " słowa"); //operator concat'enacji
+console.log("5" * 2);
+console.log("5" * "2");
+console.log("ABC" * 2);
+
+console.log(2 + 2 * 2);
+
+console.log("x = " + x);
+
+x = x + 5;
+console.log("x = " + x);
+
+x += 9;
+console.log("x = " + x);
+
+x -= 9;
+console.log("x = " + x);
+
+x *= 2;
+console.log("x = " + x);
+
+x /= 7;
+console.log("x = " + x);
+
+x %= 3;
+console.log("x = " + x);
+
+x += 1;
+console.log("x = " + x);
+
+x++; //inkrementacja (post), ++x preinkrementacja
+console.log("x = " + x);
+
+x--; //dekrementacja (post), --x predekrementacja
+console.log("x = " + x);
+
+var y = ++x;
+
+console.log("y = " + y);
